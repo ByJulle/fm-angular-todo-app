2488a31a86d0c918e20a991e0122d0c6462c168a
Julien <menten.julien@outlook.be>
2024-09-10 10:52:39 +0200

    Basic Angular start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
 
+const ROUTES = [HomeComponent];
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [...ROUTES, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-todo-app';
