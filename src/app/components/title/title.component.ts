b4caaad19f79e6230fdccfdf0900a483800de409
Julien <menten.julien@outlook.be>
2024-09-13 11:24:14 +0200

    Optimized the theme switcher

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { switchTheme } from '../../state/theme/theme.actions';
 import { THEME } from '../../types/theme';
+import { selectTheme } from '../../state/theme/theme.selectors';
+import { AppState } from '../../state/app.state';
+import { of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-title',
@@ -11,9 +14,28 @@ import { THEME } from '../../types/theme';
   styleUrl: './title.component.scss',
 })
 export class TitleComponent {
-  constructor(private store: Store) {}
+  currentTheme: THEME = THEME.LIGHT;
+  themeSubscription$!: Subscription;
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    this.themeSubscription$ = this.store
+      .select(selectTheme)
+      .subscribe((theme) => (this.currentTheme = theme));
+  }
+
+  ngOnDestroy() {
+    if (this.themeSubscription$) {
+      this.themeSubscription$.unsubscribe();
+    }
+  }
 
   toggleTheme() {
-    this.store.dispatch(switchTheme({ theme: THEME.DARK }));
+    this.store.dispatch(
+      switchTheme({
+        theme: this.currentTheme === THEME.LIGHT ? THEME.DARK : THEME.LIGHT,
+      }),
+    );
   }
 }
